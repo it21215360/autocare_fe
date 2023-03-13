9be1a3f641f788dcab3dd3299b9ccb183014bae9
shamith <shamithrosa@gmail.com>
2023-03-13 08:42:19 +0530

    payroll layout fixes

--- src/pages/payroll_hr/payroll.jsx
+++ src/pages/payroll_hr/payroll.jsx
@@ -5,25 +5,25 @@ import { useState } from "react";
 function Salary() {
 
 
-    const [salary,setSalary] = useState();
+    const [salary, setSalary] = useState();
     //var [tax,setTax] = useState();
-    var [nsal,setNsal] = useState();
-    var [allowance,setAllowance] = useState();
-    var [deductions,setDeductions] = useState();
+    var [nsal, setNsal] = useState();
+    var [allowance, setAllowance] = useState();
+    var [deductions, setDeductions] = useState();
 
-    const [COL,setCol]= useState();
-    const [Insentive,setInsentive]=useState();
-    var [EPF,SetEPF]=useState(salary *8/100);
-    var [Tax,setTax]=useState();
-    var [MedInsuarance,setMedInsuarance]=useState(300);
-    var [welfare,setWelfare]= useState(200);
-    
+    const [COL, setCol] = useState();
+    const [Insentive, setInsentive] = useState();
+    var [EPF, SetEPF] = useState(salary * 8 / 100);
+    var [Tax, setTax] = useState();
+    var [MedInsuarance, setMedInsuarance] = useState(300);
+    var [welfare, setWelfare] = useState(200);
+
     function Calculation() {
 
         COL = 5000;
         Insentive = 15000;
-        Tax = salary*8/100;
-        EPF = salary*8/100;
+        Tax = salary * 8 / 100;
+        EPF = salary * 8 / 100;
         MedInsuarance = 300;
         welfare = 200;
 
@@ -37,81 +37,71 @@ function Salary() {
 
         //allowance = salary + COL + Insentive;
         allowance = salary + COL + Insentive;
-        deductions = EPF+Tax+MedInsuarance+welfare;
-        
-      
+        deductions = EPF + Tax + MedInsuarance + welfare;
+
+
         setAllowance(allowance);
         setDeductions(deductions);
 
         nsal = allowance - deductions;
         setNsal(nsal);
 
-       // alert(salary);
-     /* if(salary > 50000)
-        {
-            tax = salary * 10/100;
+        // alert(salary);
+        /* if(salary > 50000)
+           {
+               tax = salary * 10/100;
+   
+           }
+           else if(salary > 30000)
+           {
+               tax = salary * 5/100;
+           }
+           else
+           {
+               tax = 0;
+           }
+   
+           setTax(tax);
+   
+           nsal = salary - tax;
+           setNsal(nsal);
+   */
 
-        }
-        else if(salary > 30000)
-        {
-            tax = salary * 5/100;
-        }
-        else
-        {
-            tax = 0;
-        }
-
-        setTax(tax);
-
-        nsal = salary - tax;
-        setNsal(nsal);
-*/
-        
 
     }
 
-    return(
-        <>
-            <div class="container">
+    return (
+        <React.Fragment>
+            <div className={'content-block'}>
                 <h2>Salary Calculation Page</h2>
-
-            
                 <div class="form-group">
                     <label>Employee Name</label>
-                    <input type="text" class="form-control" placeholder="Enter name"/>
-                
+                    <input type="text" class="form-control" placeholder="Enter name" />
                 </div>
                 <div class="form-group">
                     <label>Salary</label>
                     <input type="text" class="form-control" placeholder="Enter Salary"
-                    
-                    //calculating salary
-                    
-                    onChange={(event) => 
-                    {
-                        setSalary(event.target.value);
-                    }}
-                    
-                    
+
+                        //calculating salary
+                        onChange={(event) => {
+                            setSalary(event.target.value);
+                        }}
                     />
-                
                 </div>
 
                 <div class="form-group">
                     <label>Tax</label>
-                    <input type="text" class="form-control" placeholder="Tax" value={ allowance }/>
+                    <input type="text" class="form-control" placeholder="Tax" value={allowance} />
                 </div>
 
                 <div class="form-group">
                     <label>Net Salary</label>
-                    <input type="text" class="form-control" placeholder="Net Salary" value={nsal}/>
-                
+                    <input type="text" class="form-control" placeholder="Net Salary" value={nsal} />
+
                 </div>
-                
-        
-        <button type="submit"  onClick={Calculation} class="btn btn-primary mt-4">Submit</button>
-        </div>
-  </>
+                <button type="submit" onClick={Calculation} class="btn btn-primary mt-4">Submit</button>
+            </div>
+        </React.Fragment>
 
     );
 }
