f4e9472359a6841eeb7dd9e5c0ef157f884fcd87
Fernando M.A.N <it21238994@my.sliit.lk>
2023-03-18 14:01:45 +0530

    merge cinflict resolve

--- src/app-routes.js
+++ src/app-routes.js
@@ -27,26 +27,82 @@ import {
 import { withNavigationWatcher } from "./contexts/navigation";
 
 const routes = [
-    {
-        path: '/tasks',
-        element: TasksPage
-    },
-    {
-        path: '/profile',
-        element: ProfilePage
-    },
-    {
-        path: '/home',
-        element: HomePage
-    },
-    {
-        path: 'master/product-category',
-        element: ProductCategory
-    },
-    {
-        path: 'payroll_hr/emp-attendance',
-        element: Attendance
-    }
+  { path: "/tasks", element: TasksPage },
+  { path: "/profile", element: ProfilePage },
+  { path: "/home", element: HomePage },
+  { path: "master/product-category", element: ProductCategory },
+
+  //shamith
+
+  //it21215360@my.sliit.lk - Maheesha
+  { path: "payroll_hr/leave-request-form", element: LeaveRequestForm },
+  { path: "payroll_hr/emp-attendance", element: Attendance },
+  { path: "payroll_hr/emp-payroll", element: Salary },
+  { path: "payroll_hr/leaveApproval", element: LeaveApproval },
+  { path: "payroll_hr/dailyAttendanceViewer", element: AttendanceViewer },
+
+  //it21324406@my.sliit.lk - Shanoli
+  { path: "stock_management/storage", element: Storage },
+  { path: "stock_management/suppliers", element: Suppliers },
+  { path: "stock_management/order", element: Order },
+  {
+    path: "stock_management/stock-order-request-form",
+    element: PurchaseOrderForm,
+  },
+  { path: "stock_management/stock-return-form", element: StockReturnForm },
+
+  //it21197000@my.sliit.lk - Chethani
+  { path: "/scheduling/Scheduling_appointment", element: Scheduling },
+  { path: "/scheduling/Package", element: PackageDetails },
+  { path: "/scheduling/Update_appointment", element: Update },
+  { path: "/scheduling/Cancel_appointment", element: Cancel },
+
+  //it21198090@my.sliit.lk - Shania
+
+
+
+
+
+
+
+  //it21198090@my.sliit.lk - Shania
+
+
+
+
+
+
+
+
+
+  //it21307362@my.sliit.lk - Fernando ST
+  { path: "courier/CourierLogin", element: CourierLogin },
+  { path: "courier/CourierRegistration", element: CourierRegistration },
+  { path: "courier/VehicleReg", element: VehicleReg },
+  { path: "courier/DeliveryRequestForm", element: DeliveryRequestForm },
+  { path: "courier/ShippingManage", element: ShippingManage },
+  { path: "courier/CourierProfile", element: CourierProfile },
+
+  //it21238994@my.sliit.lk - Amanda
+
+
+
+
+
+
+
+
+
+  //it21326936@my.sliit.lk - Amandi
+
+
+
+
+
+
+
+
+
 ];
 
 export default routes.map((route) => {
