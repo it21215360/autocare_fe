f462bf6300604e7dbd265856a14d5c0b3931159e
Gunaratne <it21326936@my.sliit.lk>
2023-04-18 01:51:07 -0700

    Ordering and Payment changes

--- src/pages/ordering/Invoice.js
+++ src/pages/ordering/Invoice.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import 'devextreme/dist/css/dx.light.css';
+//import { IconName } from "react-icons/ci";
+import React from "react";
 import DataGrid, { Column, SearchPanel, Editing, ValidationRule } from 'devextreme-react/data-grid';
-//import { Button } from 'devextreme-react';
+//import { useState } from "react";
+//import Form, { EmptyItem, GroupItem, Item, Label } from "devextreme-react/form";
+//import { Navbar, ListGroup } from "react-bootstrap";
+import { Button } from 'devextreme-react/button';
 
+function invoice() {
 
-
-export default function Ordersdet() {
-    
-    const order = [{ AutoID: 1, ProdID: 2, ProductCatID: 5, ProductSubCatID: 3, ProductName:'ghjj', UnitPrice:'23', Quantity: 60, Brand:'ghj', CreatedDte:'2022/1/2', UpdatedDte:'2022/12/3', Rating:'4'}, 
+    const paydet = [{ AutoID: 1, ProdID: 2, ProductCatID: 5, ProductSubCatID: 3, ProductName:'ghjj', UnitPrice:'23', Quantity: 60, Brand:'ghj', CreatedDte:'2022/1/2', UpdatedDte:'2022/12/3', Rating:'4'}, 
     { AutoID: 2, ProdID: 4, ProductCatID: 2, ProductSubCatID: 3, ProductName:'rtyui', UnitPrice:'3460', Quantity: 40, Brand:'tyu', CreatedDte:'2022/12/1', UpdatedDte:'2023/3/4', Rating:'5'}]
 
     return (
@@ -15,7 +16,7 @@ export default function Ordersdet() {
             <div className={'content-block'}>
                 <h5><b>Order Details</b></h5>
                 <DataGrid id='sample'
-                    dataSource={order}
+                    dataSource={paydet}
                     rowAlternationEnabled={true}
                     showBorders={true}>
                     <SearchPanel visible={true} highlightCaseSensitive={true} />
@@ -39,9 +40,16 @@ export default function Ordersdet() {
 
                 </DataGrid>
                 <br></br>
-    
+                <div>
+                  
+                  <Button><b>Confirm</b></Button>
+                  <Button><b>Edit</b></Button>
+                </div>
+
             </div>
         </React.Fragment>
 
     )
 }
+
+export default invoice;
