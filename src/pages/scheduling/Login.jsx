6338fafecdcc550410f0218f97263bdddb56cd3b
Amanda Chethani <amandafernando0328@gmail.com>
2023-04-19 08:52:57 +0530

    logindone

--- src/pages/scheduling/Login.jsx
+++ src/pages/scheduling/Login.jsx
@@ -9,11 +9,32 @@ import axios from "axios";
 import { API_BASE_URL } from "../../appconfig/config";
 
 function ServiceLogin() {
+
+  const [userloginInfo, setUserLoginInfo] = useState({});
+  const onSaveBtnClick = (e) => {
+  try{
+    console.log(userloginInfo);
+
+    axios  
+      .post(`${API_BASE_URL}/api/customer/login-request-form`, {
+        loginInfo: JSON.stringify(userloginInfo)
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {});
+    }catch (error) {
+      console.error(error);
+    }
+};
+
+
+
   return (
     <>
       <div className={'back-box'}>
         <h2 className="head1">Service Station Management</h2>
-        <Form>
+        <Form formData={userloginInfo}>
           <GroupItem colCount={1}>
             <Item 
                 dataField="username"  
@@ -39,6 +60,24 @@ function ServiceLogin() {
 
           </GroupItem>
         </Form>
+
+        <Navbar bg="light" variant="light">
+                                <Button 
+                                    className="button1"
+                                    stylingMode="contained" 
+                                    type="success" 
+                                    onClick={onSaveBtnClick}
+                                >
+                                        Save
+                                </Button>
+                                <Button 
+                                    className="button2"
+                                    stylingMode="contained" 
+                                    type="default"
+                                >
+                                        Clear
+                                </Button>
+                                </Navbar>
         <br />
       </div>
 
