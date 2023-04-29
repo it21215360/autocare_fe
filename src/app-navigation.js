f080bbddb45e804fff16b4c83379c02da8c44fde
Amanda Chethani <amandafernando0328@gmain.com>
2023-04-29 22:27:46 +0530

    schedulingorderd

--- src/app-navigation.js
+++ src/app-navigation.js
@@ -63,22 +63,19 @@ export const navigation = [
     items: [
       { text: "Service Scheduling", path: "scheduling/Service" },
       {
-        text: "Car Wash Scheduling",
+        text: "Car Wash Scheduling Appointment",
         path: "scheduling/Scheduling_appointment",
       },
-     
       { text: "Service Manager Login", path: "scheduling/Login" },
       {
         text: "Confirm Appointment viewer",
         path: "scheduling/Confirm_appointment",
       },
-      { text: "Update Appointment", path: "scheduling/Update_appointment" },
-      
-      { text: "Cancel Appointment", path: "scheduling/Cancel_appointment" },
-      { text: "Booked memo viewer", path: "scheduling/Memo" },
       { text: "Package Details", path: "scheduling/Package" },
+      { text: "Cancel Appointment", path: "scheduling/Cancel_appointment" },
+      { text: "Update Appointment", path: "scheduling/Update_appointment" },
+      { text: "Booked memo viewer", path: "scheduling/Memo" },
       
-     
     ],
   },
 
